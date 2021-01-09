0ff4fe385db80ff257813a28836df7bc50d55f89
Chandra Chaganti <chandra.chaganti@hotmail.com>
2021-01-09 05:48:29 +0100

    sm7250-common: remove lineage trust

--- common.mk
+++ common.mk
@@ -446,10 +446,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus_lito
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
