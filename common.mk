8ba2daaf94049e09f35140839b242757881fd857
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-07 14:54:27 +0100

    sm7250-common: disable dt2w and touch gestures for now
    * Source built kernel breaks NFC

--- common.mk
+++ common.mk
@@ -441,10 +441,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oneplus_lito
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
