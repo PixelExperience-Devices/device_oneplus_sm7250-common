47d5d8ed5824556a280ac7c0bb8368313321587a
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-07-18 18:57:50 +0530

    sm7250-common: Initial common device tree conversion

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.oneplus_kona"
+#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.oneplus_lito"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
