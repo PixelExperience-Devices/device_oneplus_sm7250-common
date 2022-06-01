b72daeb7c234605a340359d91e90bb935a568406
aswin7469 <aswinas@pixysos.com>
2022-06-01 08:13:46 +0000

    sm7250-common: cleanup aosp Codec2 modules
    * we switched to  QTI Codec2 HAL
    * also build 64 bit version of codec2 libs

--- common.mk
+++ common.mk
@@ -347,8 +347,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libcodec2_hidl@1.0.vendor:32 \
-    libcodec2_vndk.vendor:32
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor
 
 # Net
 PRODUCT_PACKAGES += \
@@ -515,62 +515,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
 endif
 
-# Media
-PRODUCT_PACKAGES += \
-    libmm-omxcore \
-    android.hardware.media.c2@1.0.vendor \
-    android.hardware.media.c2@1.2.vendor
-
-# Codec2 modules
-PRODUCT_PACKAGES += \
-    com.android.media.swcodec \
-    libsfplugin_ccodec
-
-# Enable Codec 2.0
-PRODUCT_PACKAGES += \
-    libqcodec2_base \
-    libqcodec2_utils \
-    libqcodec2_platform \
-    libqcodec2_core \
-    libqcodec2_basecodec \
-    libqcodec2_v4l2codec \
-    vendor.qti.media.c2@1.0-service \
-    codec2.vendor.ext-arm64.policy \
-    codec2.vendor.base-arm64.policy
-
-# Media codecs C2
-PRODUCT_PACKAGES += \
-    codec2play \
-    libcodec2_soft_aacdec \
-    libcodec2_soft_aacenc \
-    libcodec2_soft_amrnbdec \
-    libcodec2_soft_amrwbdec \
-    libcodec2_soft_amrnbenc \
-    libcodec2_soft_amrwbenc \
-    libcodec2_soft_av1dec_aom \
-    libcodec2_soft_avcdec \
-    libcodec2_soft_avcenc \
-    libcodec2_soft_common \
-    libcodec2_soft_flacdec \
-    libcodec2_soft_flacenc \
-    libcodec2_soft_g711alawdec \
-    libcodec2_soft_g711mlawdec \
-    libcodec2_soft_av1dec_gav1 \
-    libcodec2_soft_gsmdec \
-    libcodec2_soft_hevcdec \
-    libcodec2_soft_hevcenc \
-    libcodec2_soft_mp3dec \
-    libcodec2_soft_mpeg2dec \
-    libcodec2_soft_mpeg4dec \
-    libcodec2_soft_h263dec \
-    libcodec2_soft_mpeg4enc \
-    libcodec2_soft_h263enc \
-    libcodec2_soft_opusdec \
-    libcodec2_soft_opusenc \
-    libcodec2_soft_rawdec \
-    libcodec2_soft_vorbisdec \
-    libcodec2_soft_vp9dec \
-    libcodec2_soft_vp8dec \
-    libcodec2_soft_vp9enc \
-    libcodec2_soft_vp8enc \
-    libcodec2_soft_xaacdec
