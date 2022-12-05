f37f82332313504f616d14101bf7f1834f11faf5
George Markakis <realipertatos@gmail.com>
2022-12-05 17:36:32 +0200

    Change target clang version to an r416183b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_SOURCE := kernel/oneplus/$(TARGET_DEVICE)
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig
 
 # Platform
